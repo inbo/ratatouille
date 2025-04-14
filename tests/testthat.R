57364a96ac59253c3b998ce8978356dfca3385fa
PietrH <pieterhuy@gmail.com>
2025-04-14 14:20:06 +0200

    `usethis::use_testthat()`

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(rato.occurrences)
+library(rato.data)
 
-test_check("rato.occurrences")
+test_check("rato.data")
