979f85ebe91f8be305e5bc284115e8f932e8552a
PietrH <pieterhuy@gmail.com>
2025-08-11 16:23:00 +0200

    Stub tests: test return, argument passing, error on invalid source

---
+++ tests/testthat/test-ratatouille.R
@@ -0,0 +1,27 @@
+test_that("ratatouille supports fetching data from rato", {
+  skip_if_offline(host = "gis.oost-vlaanderen.be")
+  skip_if(Sys.getenv("RATO_USER") == "")
+  skip_if(Sys.getenv("RATO_PWD") == "")
+
+  expect_s3_class(
+    ratatouille(source = "rato"),
+    "data.frame"
+  )
+})
+
+test_that("ratatouille returns error on invalid source", {
+  expect_error(
+    ratatouille(source = "not a source"),
+    regexp = "`source` must be one of"
+  )
+})
+
+test_that("ratatouille can pass on arguments to it's internal helpers", {
+  my_object_ids <- sample(list_object_ids(), size = 5)
+  
+  expect_s3_class(
+    ratatouille(object_ids = my_object_ids,
+                batch_size = 1),
+    "data.frame"
+  )
+})
