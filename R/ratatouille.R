1e94bf38fcbb94f7f056f5b9a0a573b4d0cdfd07
PietrH <pieterhuy@gmail.com>
2025-09-02 11:35:40 +0200

    reinstate token argument, start rewrite of ratatrouille() to batch internally.

--- R/ratatouille.R
+++ R/ratatouille.R
@@ -3,7 +3,8 @@
 #'
 #' @param source Character string indicating the source of the data to fetch. By
 #'   default RATO data is fetched.
-#' @param ... Passed on to internal helpers.
+#' @inheritParams get_objects
+#' @inheritDotParams get_objects object_ids batch_size
 #'
 #' @return A data.frame containing the raw invasive species data from the
 #'   selected source.
@@ -11,11 +12,23 @@
 #'
 #' @examplesIf interactive()
 #' ratatouille()
-ratatouille <- function(source = c("rato"), ...) {
-  raw_data <-
-    switch(rlang::arg_match(source),
-      rato = get_objects(...)
-    )
+ratatouille <- function(source = c("rato"),
+                        object_ids = list_object_ids(),
+                        ...) {
+  
+  # Internally batch requests per 1000 records, these are processed in parallel.
+  # If too many requests are batched, the token may expire before the requests
+  # finish.
+  internal_batch_size <- 1000
+  batched_ids <-
+    split(object_ids, ceiling(seq_along(object_ids) / internal_batch_size))
+  
+  batched_query_results <- 
+    batched_ids %>% 
+    purrr::map(\(object_ids) get_objects(object_ids, token = get_token()),
+               .progress = FALSE)
 
+  raw_data <- purrr::list_rbind(batched_query_results)
+  
   return(raw_data)
 }
