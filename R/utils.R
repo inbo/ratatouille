63b5bf5763378065125a5a956df15fec92ca354e
PietrH <pieterhuy@gmail.com>
2026-02-19 17:29:52 +0100

    Helper to get default resource per source, so it's easier to change in the future

--- R/utils.R
+++ R/utils.R
@@ -80,6 +80,31 @@ get_api_basepath <- function(source = c("rato", "wfl"),
   )
 }
 
+#' Get default resource path for a given source
+#'
+#' ArcGIS Enterprise allows queries based on the layerOrTableId, this function
+#' returns a default resource to query.
+#'
+#' @param source Character string of the source enum to look up the default resource for.
+#'
+#' @returns Character string of the default resource path corresponding to the specified
+#'  source.
+#'
+#' @examples
+#' get_default_resource("rato")
+#' @family utils
+#' @noRd
+get_default_resource <- function(source = c("rato", "wfl")) {
+  source <- rlang::arg_match(source)
+  
+  dplyr::recode_values(
+    source,
+    "rato" ~ paste("RATO2", "RATO2_Dossiers_Publiek", sep = "/"),
+    "wfl" ~ paste("Ecosystem2", "AGS_ES2_Dossiers_Publiek", sep = "/"),
+    unmatched = "error"
+  )
+}
+
 #' Create .onLoad function to set Package options and memoisation behavior on
 #' load
 #'
